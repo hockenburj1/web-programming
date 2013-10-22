aadf5bf90e45a1fbbd1588540a6a67f1f29b2d2a
Jesse Hockenbury <hockenburj1@nku.edu>
2013-10-21 23:08:38 -0400

    Beginning of the math class

---
+++ Math.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Handles a variety of mathematical functions
+ *
+ * @author hocke_000
+ */
+class Math {
+    //put your code here
+    
+    /**
+     * Add two numbers together
+     * 
+     * @param int $value1 First value to be added
+     * @param int $value2 Second value to be added
+     * 
+     * @return int Value of two integers added together,
+     */
+    
+    static function add($value1, $value2) {
+        return $value1 + $value2;
+    }
+}
+
+?>
